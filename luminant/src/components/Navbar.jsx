ced5f2be9ba798d2e628fab92281fa9f0348de12
Lucas Marinho <lucassenatore4@gmail.com>
2026-08-11 17:13:53 -0300

    ✨feat: navbar

--- luminant/src/components/Navbar.jsx
+++ luminant/src/components/Navbar.jsx
@@ -3,10 +3,12 @@ import './Navbar.css'
 function Navbar() {
   return (
     <nav className="navbar">
+        <img src="./luminantlogo2.png" className="img-logo2" alt="" />
         <Link to="/">Home</Link>
         <Link to="/pagina1">Página 1</Link>
         <Link to="/pagina2">Página II</Link>
         <Link to="/pagina3">Página três</Link>
+        <p className="ver">V1.0 termos - privacidade - suporte</p>
     </nav>
   )
 }
